519902c1106002e497fc91d1d5c1a7987eb27f6d
jiaojiner <15148365776@163.com>
2020-02-19 10:09:21 +0800

    免费ARP

---
+++ ARP/ARP_Free.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- encoding = utf-8 -*-
+# 该代码由本人学习时编写，仅供自娱自乐！
+# 本人QQ：1945962391 
+# 欢迎留言讨论，共同学习进步！
+
+from Tools import GET_IP_netifaces
+from ARP import ARP_Request
+
+
+def arp_free(ifname='ens33'):
+    localip = GET_IP_netifaces.get_ip_address(ifname)
+    pkt = ARP_Request.arp_request(localip, ifname)
+    if pkt[-1]:
+        return pkt[0], pkt[-1]
+    else:
+        return pkt[0], None
+
+
+if __name__ == '__main__':
+    result = arp_free()
+    if result[-1]:
+        print('接口地址：', result[0], '与该设备', result[-1], '地址冲突！')
+    else:
+        print('接口地址不冲突，请放心使用！')
