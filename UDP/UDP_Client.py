bef49d2ff9a2f2dc2c488cacd6a68e39c4570058
jiaojiner <15148365776@163.com>
2020-02-20 11:22:06 +0800

    实现简易UDP客户端及服务端通信

---
+++ UDP/UDP_Client.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- encoding = utf-8 -*-
+# 该代码由本人学习时编写，仅供自娱自乐！
+# 本人QQ：1945962391 
+# 欢迎留言讨论，共同学习进步！
+
+import socket
+import sys
+import time
+
+server_ip = input('请输入需要连接UDP服务端的IP地址:')
+server_port = input('请输入需要连接UDP服务端的端口号:')
+ipaddress = (server_ip, int(server_port))  # 设置需要连接的服务端地址与端口
+udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # 创建udp连接
+udp_socket.connect(ipaddress)  # 连接服务端
+
+while True:
+    try:
+        data = input('请输入需要传输的数据：')
+        if not data:  # 如果无数据输入，发送空数据，并退出循环
+            udp_socket.sendto(data.encode(), ipaddress)
+            print('无数据输入，程序即将退出！')
+            time.sleep(1)
+            break
+        else:  # 否则传输数据，继续循环
+            udp_socket.sendto(data.encode(), ipaddress)
+            print('数据传输完毕！')
+    except KeyboardInterrupt:  # 如客户输入ctrl+c，打印提示信息，退出程序
+        print('用户手动退出程序！')
+        sys.exit()
+udp_socket.close()
---
+++ UDP/UDP_Server.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- encoding = utf-8 -*-
+# 该代码由本人学习时编写，仅供自娱自乐！
+# 本人QQ：1945962391 
+# 欢迎留言讨论，共同学习进步！
+
+from Tools import GET_IP_netifaces
+import socket
+import sys
+import time
+
+localip = GET_IP_netifaces.get_ip_address(ifname='ens33')
+ipadress = (localip, 8888)
+udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # 创建udp的socket套接字
+udp_socket.bind(ipadress)  # 绑定ip地址及端口
+
+print('Server端已准备就绪！等待数据传输')
+
+while True:
+    try:
+        data, ipaddr = udp_socket.recvfrom(2048)
+        if not data:
+            print('客户端程序已退出！服务端即将断开')
+            time.sleep(1)
+            break
+        print('接收来自', ipaddr, '的数据！', '传输内容为：', data)
+    except KeyboardInterrupt:
+        print('服务端准备退出！')
+        time.sleep(1)
+        sys.exit()
+udp_socket.close()
